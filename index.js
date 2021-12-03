f93f43b720333fc8ec94f8a3aec796255b5bda5d
Ivo Janssen <ivo@appliedminds.com>
2021-12-03 11:14:31 -0800

    feat: always use latest ECMAScript version to lint against

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
       "eslint:recommended"
     ],
     "parserOptions": {
-      "ecmaVersion": 2020,
+      "ecmaVersion": "latest",
       "sourceType": "module"
     },
     "rules": {
