53b05b28163bf5ead93953496e28ba2d4f8a7283
Ivo Janssen <ivo@appliedminds.com>
2023-07-18 10:58:22 -0700

    fix: mutating properties of props passed reference (where the reference remains intact) should not throw errors; use new `shallowOnly` option

--- vue.js
+++ vue.js
@@ -9,6 +9,7 @@ module.exports = {
     "rules": {
       "vue/html-indent": ["error", 4],
       "vue/max-attributes-per-line": ["error", { "singleline": { "max": 3 } }],
+	"vue/no-mutating-props": ["error", { "shallowOnly": true }],
       "vue/multi-word-component-names": "off"
     }
 }
