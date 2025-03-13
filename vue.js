ff548a05b48d7584592fe8a2530228dd48b6ffef
Ivo Janssen <ivo@appliedminds.com>
2025-03-13 15:26:13 -0700

    fix: re-include browser globals as eslint-plugin-vue has removed them

--- vue.js
+++ vue.js
@@ -1,8 +1,12 @@
+import globals from 'globals'
 import pluginVue from 'eslint-plugin-vue'
 
 export default [
     ...pluginVue.configs['flat/strongly-recommended'],
     {
+        languageOptions: {
+            globals: globals.browser,
+        },
         rules: {
             "vue/html-indent": ["error", 4],
             "vue/max-attributes-per-line": ["error", { "singleline": { "max": 3 } }],
